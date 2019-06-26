0b65be21b76b5856fcafbdf58566f8fd6c8056d7
Vyacheslav Dyakov <31534670+AntForm@users.noreply.github.com>
2019-06-26 20:50:21 +0700

    Update tasks.py

--- olx/tasks.py
+++ olx/tasks.py
@@ -7,12 +7,6 @@ from django.core.mail import send_mail
 from olx.models import OlxRequest
 
 @shared_task
-def send_email(mail_subject, message, to_email):
-    email = EmailMessage(mail_subject, message, 'noreply@medlogic.online', [to_email])
-    email.send()
-    return 1
-
-@shared_task
 def get_request(request_id):
     model = OlxRequest.objects.get(pk = request_id)
     dic = {'января' : '1', 'февраля' : '2', 'марта': '3', 'апреля' : '4', 'мая' : '5', 'июня' : '6', 'июля' : '7', 'августа' : '8', 'сентября' : '9', 'октября' : '10', 'ноября' : '11', 'декабря': '12'}
